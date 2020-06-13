e0312857123641e8759bfedb84a334c1e2109f5e
Ruin0x11 <ipickering2@gmail.com>
2020-06-13 03:57:55 -0700

    Add secret.example.exs

---
+++ config/secret.example.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :extwitter, :oauth, [
+   consumer_key: "",
+   consumer_secret: "",
+   access_token: "",
+   access_token_secret: ""
+]
+config :to_booru, pixiv_username: ""
+config :to_booru, pixiv_password: ""
